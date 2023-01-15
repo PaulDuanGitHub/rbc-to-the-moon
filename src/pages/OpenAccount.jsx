695162a342d9597634ac2d7248b97fcb70c5c74c
part-canadian <venkat.muthaly@outlook.com>
2023-01-15 03:16:18 -0500

    here it is frfr

--- src/pages/OpenAccount.jsx
+++ src/pages/OpenAccount.jsx
@@ -8,14 +8,44 @@ class OpenAccount extends Component {
     }
     render(){
         return(<div>
+
+    
+
+    
             <h2 className= "titleba">
                 Open a Bank Account
+                
             </h2>
-            <div className="subtitlearea">
+            <div class="box"></div>
+            <h2 className="subtitleba">What We Can Offer Newcomers</h2>
+
+            <h3 className="heading">Settle in faster with the RBC Newcomer Advantage</h3>
+
+            <h7 className="sentence" >
+                • Start with a Canadian Bank Account
+                • Add a Canadian Credit Card <br/>
+                • Safe Deposit Box
+            </h7>
+            <div class="outline"></div>
+            <div class="bluebutton"></div>
+            <div class="name"><input type="text" name="" id="name-input" /></div>
+            <div class="nameword" margin="left">Name*</div>
+            <div class="email"><input type="text" name="" id="email-input" /></div>
+            <div class="emailword">Email*</div>
+            <div class="phone"><input type="text" name="" id="phone-input" font-size="12px "/></div>
+            <div class="phoneword">Phone Number</div>
+
+            <div class="outline2"></div>
+            <div class="outline2text">Tell us a little about yourself and an RBC Business Banking Specialist will contact you within one business day.</div>
+            { <h1 class="buttonbook"><NavLink to="/su"><Button size='lg' variant="Book Appointment" onClick={this.test}>Book Appointment</Button></NavLink></h1> } <br />
+
+            { <h1 class="backpage"><NavLink to = "/en"><Button size='lg' variant="Back to Home Page" onClick={this.test}>Back to Home Page</Button></NavLink></h1> }
+            
+            {/* <div className="subtitlearea">
                 <h2 className="subtitle">What We Can Offer Newcomers</h2>
-                {/* <div class="rectangleba"></div> */}
-                <h7 className="subba">Settle in faster with the new RBC Newcomer Advantage<br /></h7>
-            </div>
+                <div class="rectangleba"></div> */}
+                {/* <h7 className="subba">Settle in faster with the new RBC Newcomer Advantage<br /></h7>
+            </div> */}
 
             {/* <ul>
                 <li>Item 1</li>
--- src/pages/RecieveMoney.jsx
+++ src/pages/RecieveMoney.jsx
@@ -3,36 +3,62 @@ import { NavLink } from "react-router-dom";
 import Button from 'react-bootstrap/Button';
 
 class RecieveMoney extends Component {
+    test = () => {
+        return ;
+    }
     render(){
         return(<div>
-            <h1>
-                How to Send & Recieve Money From Home
+            <h2 className= "titlere">
+            How to Send & Recieve Money Internationally
+            </h2>
+            <div class="box"></div>
+            <h2 className="subtitleba">International Money Transfers</h2>
+
+            <h3 className="heading">Settle in faster with the RBC Newcomer Advantage</h3>
+
+            <h7 className="sentence" >
+                • Send money any time, day or night<br />
+                • Send to over 120 countries <br/>
+                • Easy and secure transfers through RBC Online Banking or the RBC Mobile app
+            </h7>
+            <div class="outline"></div>
+            <div class="bluebutton"></div>
+            <div class="name"><input type="text" name="" id="name-input" /></div>
+            <div class="nameword" margin="left">Name*</div>
+            <div class="email"><input type="text" name="" id="email-input" /></div>
+            <div class="emailword">Email*</div>
+            <div class="phone"><input type="text" name="" id="phone-input" font-size="12px "/></div>
+            <div class="phoneword">Phone Number</div>
+
+            <div class="outline2"></div>
+            <div class="outline2text">Tell us a little about yourself and start sending and receiving money internationally today!</div>
+            { <h1 class="buttonbook"><NavLink to="/su"><Button size='lg' variant="Book Appointment" onClick={this.test}>Book Appointment</Button></NavLink></h1> } <br />
+
+            { <h1 class="backpage"><NavLink to = "/en"><Button size='lg' variant="Back to Home Page" onClick={this.test}>Back to Home Page</Button></NavLink></h1> }
+            
+            
+
+            
+
+        </div>);
+        }
+    }
+
+export default RecieveMoney
+
+/*
+2 Free International Money Transfers per Month for a Year</h4>
+                Send money any time, day or night<br />
+                Send to over 120 countries<br />
+                Easy and secure transfers through RBC Online Banking or the RBC Mobile app
+
+
+                <h1>
+                How to Send & Recieve Money Internationally
             </h1>
             <div>
                 <h3>
                     International Money Transfer
                 </h3>
             </div>
-            <h4>2 Free International Money Transfers per Month for a Year</h4>
-                Send money any time, day or night<br />
-                Send to over 120 countries<br />
-                Easy and secure transfers through RBC Online Banking or the RBC Mobile app
-
-            <h1>Appointment Form <br /></h1>
-            <h3>Name:<br />
-                <input type="text" name="" id="" />
-            </h3>
-            <h3>Phone Number:<br /></h3>
-                <input type="text" name="" id="" />
-            <h3>Email:<br /></h3>
-                <input type="text" name="" id="" />
-            { <h1><NavLink to="/su"><Button size='lg' variant="Submit" onClick={this.test}>Submit</Button></NavLink></h1> } <br />
-
-            { <h1><NavLink to = "/en"><Button size='lg' variant="Back" onClick={this.test}>Back</Button></NavLink></h1> }
-
-            </div>);
-    }
-    
-}
-
-export default RecieveMoney
+*/
